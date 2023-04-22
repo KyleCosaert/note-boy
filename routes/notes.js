f5546502f44fb6893ecdd861d164b699095ec20e
Kyle Cosaert <kylecosaert@gmail.com>
2023-04-22 14:40:47 -0700

    clean up work

---
+++ routes/notes.js
@@ -0,0 +1,43 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new Note
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a Note`);
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+        title,
+        text,
+        id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding Note');
+  }
+});
+
+notes.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== id);
+      writeToFile('./db/db.json', result);
+      res.json(`Item ${id} has been deleted 🗑️`);
+    });
+});
+
+module.exports = notes;
